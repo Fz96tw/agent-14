ef22d6545f61431ae698bb40323e6eb56aa23d28
Nadeem <fz96tw@gmail.com>
2026-05-04 10:55:53 -0400

    [AGENT-18] Integrate Calculation Logic with Input

--- calculator.sh
+++ calculator.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
 
-# Validate number of arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: calc.sh <operand1> <operator> <operand2>"
-    exit 1
+function validate_input() {
+  if [ $# -ne 3 ]; then
+    echo "Error: Usage: calculator.sh <operand1> <operator> <operand2>"
+    return 1
+  fi
+
+  local operand1=$1
+  local operator=$2
+  local operand2=$3
+
+  if ! [[ $operand1 =~ ^-?[0-9]+$ ]] || ! [[ $operand2 =~ ^-?[0-9]+$ ]]; then
+    echo "Error: Operands must be valid numbers"
+    return 1
+  fi
+
+  if ! [[ "$operator" =~ ^[\+\-\*/]$ ]]; then
+    echo "Error: Operator must be +, -, *, or /"
+    return 1
+  fi
+
+  return 0
+}
+
+function calculate() {
+  local operand1=$1
+  local operator=$2
+  local operand2=$3
+
+  case $operator in
+    +)
+      echo $(($operand1 + $operand2))
+      ;;
+    -)
+      echo $(($operand1 - $operand2))
+      ;;
+    \*)
+      echo $(($operand1 * $operand2))
+      ;;
+    /)
+      if [ $operand2 -eq 0 ]; then
+        echo "Error: Division by zero"
+      else
+        echo $(($operand1 / $operand2))
+      fi
+      ;;
+    *)
+      echo "Error: Unsupported operator"
+      ;;
+  esac
+}
+
+validate_input "$@"
+if [ $? -ne 0 ]; then
+  exit 1
 fi
 
-# Assign command line arguments to variables
-operand1="$1"
-operator="$2"
-operand2="$3"
-
-# Validate operand types
-if ! [[ "$operand1" =~ ^-?[0-9]+$ ]] || ! [[ "$operand2" =~ ^-?[0-9]+$ ]]; then
-    echo "Error: Operands must be integers"
-    exit 1
-fi
-
-# Validate operator
-if ! [[ "$operator" =~ ^[+\-*/]$ ]]; then
-    echo "Error: Unsupported operator"
-    exit 1
-fi
-
-# Output validated inputs (for debugging)
-echo "Operands: $operand1, $operand2; Operator: $operator"
-
-# Call the calculation function (assumed to be defined later)
-# calculate $operand1 $operator $operand2
+calculate $1 $2 $3
